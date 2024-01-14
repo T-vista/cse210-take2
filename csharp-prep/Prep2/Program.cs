cd3730c825afa290cb30b97564087c7215910e00
T-vista <tylervistaunet@gmail.com>
2024-01-13 19:43:18 -0700

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Specialized;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string number = Console.ReadLine();
+        int percentage = int.Parse(number);
+
+        string grade = " ";
+        bool pass = false ;
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+            pass = true;
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+            pass = true;
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+            pass = true;
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (pass == true)
+        {
+            Console.WriteLine("Congradulations, you passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, you did not pass");
+        }
     }
 }
