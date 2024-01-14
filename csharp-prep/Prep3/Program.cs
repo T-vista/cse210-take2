fc4e6e5db2f6e0e8b8fbea22bb58a1ef87ebd732
T-vista <tylervistaunet@gmail.com>
2024-01-13 20:31:52 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is your magic number");
+        // string num = Console.ReadLine();
+        // int magicnum = int.Parse(num); 
+        Random randomGenerator = new Random();
+        int magicnum = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (magicnum != guess)
+        {
+            Console.WriteLine("what is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicnum > guess)
+            { 
+                Console.WriteLine("Higher");
+            }
+            else if (magicnum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+        }
     }
 }
