0f5626be3af84a004931b85bc40c4ed9a6157dd6
T-vista <tylervistaunet@gmail.com>
2024-01-20 23:55:31 -0700

    journal project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1: Create Entry");
+        Console.WriteLine("2: Display All Entries");
+        Console.WriteLine("3: Save Entry");
+        Console.WriteLine("4: Load Entry:");
+        Console.WriteLine("5: Quit");
     }
 }
