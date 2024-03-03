2b7b492fa4801972de8d0d4dc9f2ec79b8cb1f65
T-vista <tylervistaunet@gmail.com>
2024-03-02 22:25:40 -0700

    rest of prep work

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,35 +6,40 @@ class Program
     {
         DisplayWelcome();
 
-        string _userName = PromptUserName();
-        int _userNumber = PromptUserNumber();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
 
-        int _newNumber = SquareNumber(_userNumber);
-        DisplayResult(_userName, _newNumber);
+        int newNumber = SquareNumber(userNumber);
+        DisplayResult(userName, newNumber);
     }    
     static void DisplayWelcome()
     {
-        Console.WriteLine("Welcome to the program!");
+        //welcome message
+        Console.WriteLine("Welcome to the program! Follow the prompts!");
     }
     static string PromptUserName()
     {
+        //ask for user name
         Console.Write("Please enter your name: ");
         string _name = Console.ReadLine();
         return _name;
     }
     static int PromptUserNumber()
     {
+        // get a number from the user
         Console.Write("Please enter your favorite number: ");
         int _number = int.Parse(Console.ReadLine());
         return _number;
     }
     static int SquareNumber(int num1)
     {
+        //square the number and return result
         int _squaredNumber = num1 * num1;
         return _squaredNumber;
     }
     static void DisplayResult(string name, int number)
     {
+        //print the result of the exercise
         Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
